d5310f458220d2323a17594e5cf13d0fc16b7fcf
hussein-kandeel <hussein.mahmoud.s7s@gmail.com>
2023-05-17 22:44:45 +0300

    Added Feature: removing duplicates from PATH environment variable

--- src/pathvar/project.py
+++ src/pathvar/project.py
@@ -1,5 +1,54 @@
-def main():
-    print("This is pathvar!")
+import os
+
+# TODO: Delete the following constants if there is no need to them
+HOME_PATH: str = os.path.normpath(os.path.expanduser('~'))
+TEMP_PATH: str = os.path.join(HOME_PATH, "__TEMP_current_path_var__.txt")
+
+
+def main() -> None:
+
+    # Place holder for the current path
+    current_path: str = os.environ["PATH"]
+
+    # Print the current state
+    print(format_message("echo Eliminating duplicates..."))
+    # Filter the path variable
+    new_path: str = str_duplicates_elimination(current_path)
+
+    # Print the current state
+    print(format_message(f"Old: {current_path}",
+                         f"New: {new_path}",
+                         f"IsAnyDuplicateElimination: {current_path != new_path}"))
+
+    # TODO: Modify os.environ["PATH"] to hold the new clean value
+
+
+def str_duplicates_elimination(s: str) -> str:
+
+    # Split on ':'
+    ss: list[str] = s.split(':')
+    if len(ss) < 2:
+        return s
+
+    # Store only the unique paths
+    temp_ss: list[str] = []
+    for p in ss:
+        if p not in temp_ss:
+            temp_ss.append(p)
+
+    # Concatenate the string result
+    result: str = ""
+    for part in temp_ss:
+        result += part + ':' if part != temp_ss[-1] else part
+    # Return the final result string without duplicates
+    return result
+
+
+def format_message(*args: str) -> str:
+    result: str = ""
+    for s in args:
+        result += '\n' + s.strip() + '\n'
+    return '\n' + result.strip()
 
 
 if __name__ == "__main__":
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,1 +1,14 @@
-from pathvar.project import *
+from src.pathvar.project import *
+
+
+inp = "/home/hussein/.cache/activestate/bin:/home/hussein/.local/ActiveState/StateTool/release/bin:/home/hussein/.cache/activestate/bin:/home/hussein/.local/ActiveState/StateTool/release/bin:/usr/bin/nodejs/node-v16.16.0-linux-x64/bin:/usr/bin/nodejs/node-v16.16.0-linux-x64/bin:/home/hussein/.local/bin:/usr/bin/nodejs/node-v16.16.0-linux-x64/bin:/usr/bin/nodejs/node-v16.16.0-linux-x64/bin:/home/hussein/.local/bin:/usr/bin/nodejs/node-v16.16.0-linux-x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/usr/lib/jvm/java-17-oracle/bin:/usr/lib/jvm/java-17-oracle/db/bin:/usr/share/rvm/bin:/usr/lib/jvm/java-17-oracle/bin:/usr/lib/jvm/java-17-oracle/db/bin:/usr/lib/jvm/java-17-oracle/bin:/usr/lib/jvm/java-17-oracle/db/bin"
+out = "/home/hussein/.cache/activestate/bin:/home/hussein/.local/ActiveState/StateTool/release/bin:/usr/bin/nodejs/node-v16.16.0-linux-x64/bin:/home/hussein/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-17-oracle/bin:/usr/lib/jvm/java-17-oracle/db/bin:/usr/share/rvm/bin"
+
+
+def test_str_duplicates_elimination():
+    assert str_duplicates_elimination(
+        "x:x:y:y:z:z:xy:xz:yz:xyz:X:Y:Z:XY:XZ:YZ:XYZ:X:Y:Z"
+    ) == "x:y:z:xy:xz:yz:xyz:X:Y:Z:XY:XZ:YZ:XYZ"
+    assert str_duplicates_elimination(inp) == out
+    assert str_duplicates_elimination(out) == out
+    assert str_duplicates_elimination('') == ''
